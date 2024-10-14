af156217fafe3cf6578ff7151ed97cebc94e536d
Ilias <ilias.elkhalloufi@edsn.nl>
2024-10-14 17:24:45 +0200

    update docs and build job

--- deploy-adot/cleanup-ssm.sh
+++ deploy-adot/cleanup-ssm.sh
@@ -6,7 +6,8 @@ aws amp delete-workspace --workspace-id $WORKSPACE_ID
 #
 # Delete SSM parameters
 #
-aws ssm delete-parameter --name otel-collector-config 
+aws ssm delete-parameter --name otel-collector-config
+# NOT used in this fork, as the value that is injected in DISCOVERY_NAMESPACES_PARAMETER_NAME should be the value of the namespace
 aws ssm delete-parameter --name ECS-Namespaces
 
 
--- deploy-adot/otel-config.sh
+++ deploy-adot/otel-config.sh
@@ -7,6 +7,7 @@ sed -e s/WORKSPACE/$WORKSPACE_ID/g \
 > otel-collector-config.yaml
 
 aws ssm put-parameter --name otel-collector-config --value file://otel-collector-config.yaml --type String
+# NOT used in this fork, as the value that is injected in DISCOVERY_NAMESPACES_PARAMETER_NAME should be the value of the namespace
 aws ssm put-parameter --name ECS-Namespaces --value ecs-services --type StringList
 
 export WORKSPACE_ID
